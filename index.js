23e04348457d99fdc3ce242ed653da52fdb7c9a9
Federico-Zorzi <federico.zorzi05@gmail.com>
2024-11-15 12:36:35 +0100

    new scripts edited (start , dev)

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+/* const nodePath = process.argv[0];
+const javascriptPath = process.argv[1];
+const param = process.argv[2];
+
+console.log(nodePath);
+console.log(javascriptPath);
+console.log(param);
+ */
+
 console.log("Hello world");
